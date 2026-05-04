c4f8af455b93eb98c3e83059f1a0b05857846c71
mr-no0b <raihanarefin.cse@gmail.com>
2026-05-04 11:09:52 +0600

    for ndu

--- code/Data structure/bit.cpp
+++ code/Data structure/bit.cpp
@@ -1,24 +1,41 @@
-template <class T>
-struct BIT { //1-indexed
-  int n; vector<T> t;
-  BIT() {}
-  BIT(int _n) {
-    n = _n; t.assign(n + 1, 0);
-  }
-  T prefixSum(int i) {
-    T ans = 0;
-    for (; i >= 1; i -= (i & -i)) ans += t[i];
-    return ans;
-  }
-  void upd(int i, T val) {
-    if (i <= 0) return;
-    for (; i <= n; i += (i & -i)) t[i] += val;
-  }
+struct FenwickTree {
+    int n;
+    int LOGN;
+    vector<ll> bit;
+    // all is 1 based
+    FenwickTree(int _n) : n(_n), bit(_n + 1, 0) {
+        LOGN = 31 - __builtin_clz(n);
+    }
+    // make A[i] += v
+    // for making A[i] = v, use +(v - A[i])
+    void update(int id, ll v) {
+        for (; id <= n; id += id & -id) {
+            bit[id] += v;
+        }
+    }
+    // Return sum of elements from 1 to idx
+    ll query(int id) {
+        ll sum = 0;
+        for (; id > 0; id -= id & -id) {
+            sum += bit[id];
+        }
+        return sum;
+    }
+    // Return sum of elements from l to r
+    ll query(int l, int r) {
+        return query(r) - query(l - 1);
+    }
+    // This is equivalent to calculating lower_bound on prefix sums array
+    // Finds the smallest index `idx` such that query(idx) >= v
+    int bit_search(ll val) {
+        ll sum = 0;
+        int pos = 0;
+        for (int i = LOGN; i >= 0; i--) {
+            if (pos + (1 << i) <= n && sum + bit[pos + (1 << i)] < val) {
+                sum += bit[pos + (1 << i)];
+                pos += (1 << i);
+            }
+        }
+        return pos + 1; // +1 because 'pos' will have position of largest value less than 'v'
+    }
 };
-// inversion 
-BIT<ll> bit(n);
-for(int i = 1; i <= n; i++) {
-    int x; cin >> x;
-    ans[i] = bit.query(n) - bit.query(x - 1);
-    bit.upd(x, 1);
-}
